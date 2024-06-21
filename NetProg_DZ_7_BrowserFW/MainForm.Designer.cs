9e50b07bf45a000f8c354e9488c41b6feca570c9
Vetal <vvvv@vvvv.vv>
2024-06-21 18:14:58 +0300

    Add project files.

---
+++ NetProg_DZ_7_BrowserFW/MainForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace NetProg_DZ_7_BrowserFW
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBoxUri = new System.Windows.Forms.TextBox();
+			this.comboBoxSearchSystem = new System.Windows.Forms.ComboBox();
+			this.buttonSearch = new System.Windows.Forms.Button();
+			this.webBrowserMain = new System.Windows.Forms.WebBrowser();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.tableLayoutPanel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 1;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.webBrowserMain, 0, 1);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.ColumnCount = 4;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.20151F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 56.42317F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.3728F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.87657F));
+			this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+			this.tableLayoutPanel2.Controls.Add(this.textBoxUri, 1, 0);
+			this.tableLayoutPanel2.Controls.Add(this.comboBoxSearchSystem, 2, 0);
+			this.tableLayoutPanel2.Controls.Add(this.buttonSearch, 3, 0);
+			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 1;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(794, 29);
+			this.tableLayoutPanel2.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label1.Location = new System.Drawing.Point(3, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(75, 29);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Запрос";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// textBoxUri
+			// 
+			this.textBoxUri.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.textBoxUri.Location = new System.Drawing.Point(84, 3);
+			this.textBoxUri.Name = "textBoxUri";
+			this.textBoxUri.Size = new System.Drawing.Size(442, 20);
+			this.textBoxUri.TabIndex = 1;
+			// 
+			// comboBoxSearchSystem
+			// 
+			this.comboBoxSearchSystem.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.comboBoxSearchSystem.FormattingEnabled = true;
+			this.comboBoxSearchSystem.Items.AddRange(new object[] {
+            "Google",
+            "Yandex",
+            "Bing"});
+			this.comboBoxSearchSystem.Location = new System.Drawing.Point(532, 3);
+			this.comboBoxSearchSystem.Name = "comboBoxSearchSystem";
+			this.comboBoxSearchSystem.Size = new System.Drawing.Size(124, 21);
+			this.comboBoxSearchSystem.TabIndex = 2;
+			// 
+			// buttonSearch
+			// 
+			this.buttonSearch.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.buttonSearch.Location = new System.Drawing.Point(662, 3);
+			this.buttonSearch.Name = "buttonSearch";
+			this.buttonSearch.Size = new System.Drawing.Size(129, 23);
+			this.buttonSearch.TabIndex = 3;
+			this.buttonSearch.Text = "Поиск";
+			this.buttonSearch.UseVisualStyleBackColor = true;
+			this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+			// 
+			// webBrowserMain
+			// 
+			this.webBrowserMain.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.webBrowserMain.Location = new System.Drawing.Point(3, 38);
+			this.webBrowserMain.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowserMain.Name = "webBrowserMain";
+			this.webBrowserMain.Size = new System.Drawing.Size(794, 409);
+			this.webBrowserMain.TabIndex = 1;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Name = "MainForm";
+			this.Text = "ПикоБраузер";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel2.ResumeLayout(false);
+			this.tableLayoutPanel2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBoxUri;
+		private System.Windows.Forms.ComboBox comboBoxSearchSystem;
+		private System.Windows.Forms.Button buttonSearch;
+		private System.Windows.Forms.WebBrowser webBrowserMain;
+	}
+}
+
---
+++ NetProg_DZ_7_BrowserFW/MainForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetProg_DZ_7_BrowserFW
+{
+
+	public partial class MainForm : Form
+	{
+		string GoogleSearch = "https://www.google.ru/search?q=%D0%BF%D1%80%D0%B8%D0%B2%D0%B5%D1%82";
+		string YandexSearch = "https://ya.ru/search/?text=";
+		string BingSearch = "https://www.bing.com/search?q=";
+		public MainForm()
+		{
+			InitializeComponent();
+			comboBoxSearchSystem.SelectedIndex = 0;
+		}
+
+		private void buttonSearch_Click(object sender, EventArgs e)
+		{
+			webBrowserMain.ScriptErrorsSuppressed = true;
+			if (comboBoxSearchSystem.SelectedIndex == 0)
+			{				
+				string request = GoogleSearch+textBoxUri.Text;
+				webBrowserMain.Navigate(request);
+			}
+			if (comboBoxSearchSystem.SelectedIndex == 1)
+			{
+				string request = YandexSearch + textBoxUri.Text;
+				webBrowserMain.Navigate(request);
+			}
+			if (comboBoxSearchSystem.SelectedIndex == 2)
+			{
+				string request = BingSearch + textBoxUri.Text;
+				webBrowserMain.Navigate(request);
+			}
+		}
+	}
+
+}
